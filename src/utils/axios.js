9a9f3e259e7674844608d2a11564e6762cbf0725
juandavid9611 <jd_rodrigueza@javeriana.edu.co>
2024-03-08 11:58:08 -0500

    Add token to each backend request

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { Auth } from '@aws-amplify/auth';
 
 import { HOST_API } from 'src/config-global';
 
@@ -6,10 +7,26 @@ import { HOST_API } from 'src/config-global';
 
 const axiosInstance = axios.create({ baseURL: HOST_API });
 
+const authTokenInterceptor = (config) => {
+  if (config.thirdParty === true) {
+    return config;
+  }
+  return Auth.currentAuthenticatedUser().then((user) => {
+    if (user) {
+      const token = user.signInUserSession.idToken.jwtToken;
+      config.headers.Authorization = `Bearer ${token}`;
+      return Promise.resolve(config);
+    }
+    return config;
+  });
+};
+
 axiosInstance.interceptors.response.use(
   (res) => res,
   (error) => Promise.reject((error.response && error.response.data) || 'Something went wrong')
 );
+
+axiosInstance.interceptors.request.use(authTokenInterceptor, (error) => Promise.reject(error));
 
 export default axiosInstance;
 
