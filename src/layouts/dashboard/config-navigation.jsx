7b1348c49dbbf09b5d6d12c631c952faed6c0e96
juandavid9611 <jd_rodrigueza@javeriana.edu.co>
2024-04-03 09:46:22 -0500

    Temporaly disable analytics

--- src/layouts/dashboard/config-navigation.jsx
+++ src/layouts/dashboard/config-navigation.jsx
@@ -4,8 +4,6 @@ import { paths } from 'src/routes/paths';
 
 import { useTranslate } from 'src/locales';
 
-import Label from 'src/components/label';
-import Iconify from 'src/components/iconify';
 import SvgColor from 'src/components/svg-color';
 
 // ----------------------------------------------------------------------
@@ -65,13 +63,15 @@ export function useNavData() {
           },
           {
             title: t('analytics'),
-            path: paths.dashboard.general.analytics,
+            path: '#analytics',
+            // path: paths.dashboard.general.analytics,
             icon: ICONS.analytics,
-            info: (
-              <Label color="info" startIcon={<Iconify icon="solar:bell-bing-bold-duotone" />}>
-                NEW
-              </Label>
-            ),
+            // info: (
+            //   <Label color="info" startIcon={<Iconify icon="solar:bell-bing-bold-duotone" />}>
+            //     NEW
+            //   </Label>
+            // ),
+            disabled: true,
           },
         ],
       },
